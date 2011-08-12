eea67fbaf8bd2f4d0d96fc90d25e04e737e9f91c
David Radcliffe <david@etchdev.com>
2011-08-12 09:13:54 -0400

    basic test

--- test/test_lotus_notes_calendar.rb
+++ test/test_lotus_notes_calendar.rb
@@ -1,7 +1,13 @@
 require 'helper'
 
 class TestLotusNotesCalendar < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  
+  should "get current events" do
+    calendar = LotusNotesCalendar::Calendar.new("https://harmony.mcsc.k12.in.us/msstudent.nsf/school+calendar")
+    results = calendar.where
+    assert_equal 26, results.count
+    assert_equal "7th Gr. First Football Practice - 9-11am", results[0].text
+    assert_equal "Monday, Aug  1", results[0].at.strftime('%A, %b %e')
   end
+  
 end
